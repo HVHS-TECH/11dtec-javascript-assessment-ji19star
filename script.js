c604016395670eaaf7dd8a03b4d2e8bff650bb49
Anahera Ilari <24243ai@hvhs.school.nz>
2026-06-02 07:48:22 +0000

    Jun 2, 2026, 7:48 AM

--- script.js
+++ script.js
@@ -13,8 +13,6 @@ let purchaseList = [];
 //Money
 const MONEY_FIELD = document.getElementById("moneyField");
 
-
-
 //
 
 /**********
@@ -36,6 +34,7 @@ const purchItem = function () {
     console.log(itemPurch);
     console.log(userMoney);
 
+    //should be the name of item, and price .. can i get it going auto when add
     purchaseList.push({
         money: userMoney,
         name: itemPurch,
@@ -44,12 +43,11 @@ const purchItem = function () {
     document.getElementById("cart").innerHTML = calcTotal(purchaseList)
 }
 
+// calculaates the price of things added to the cart and prints onto html
 const calcTotal = () => {
     let total = 0;
-
     purchaseList.forEach((item) => {
         total += parseFloat(item.money);
     });
-
     return total
 }
