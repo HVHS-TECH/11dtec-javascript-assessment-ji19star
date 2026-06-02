e8b433066f54654dddfaa1928af52b5d0be64703
Anahera Ilari <24243ai@hvhs.school.nz>
2026-06-02 06:34:48 +0000

    Jun 2, 2026, 6:34 AM

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
+
 console.log("Hello world!")
+
+
+/**********
+VARIABLES
+***********/
+
+const PURCH_FIELD = document.getElementById("purchaseField");
+
+
+/**********
+MAIN CODE
+***********/
+
+
+
+
+
+/**********
+FUNCTIONS
+***********/
+
+function purchItem(){
+
+
+}
