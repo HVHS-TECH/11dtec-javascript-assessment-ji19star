f4be1800d031a409eae0e8c023c516d3417458f1
ji19star <24243ai@hvhs.school.nz>
2026-06-03 13:30:38 +1200

    dddddd

--- script.js
+++ script.js
@@ -56,26 +56,7 @@ PRODUCTS_CONTAINER.innerHTML = products;
 /**********
 FUNCTIONS
 ***********/
-
-// document.getElementById("breadListings").innerHTML = PRODUCTS;
-
-// const purchItem = function () {
-//     //define inside of function to set, bc when it runs the function it becomes that thing
-//     let itemPurch = PURCH_FIELD.value;
-//     let userMoney = MONEY_FIELD.value;
-//     console.log(itemPurch);
-//     console.log(userMoney);
-
-//     //should be the name of item, and price .. can i get it going auto when add
-//     PURCHASE_LIST.push({
-//         money: userMoney,
-//         name: itemPurch,
-//     });
-
-//     document.getElementById("cart").innerHTML = calcTotal(PURCHASE_LIST)
-// }
-
-// calculaates the price of things added to the cart and prints onto html
+//anon function
 const calcTotal = () => {
     let total = 0;
     PURCHASE_LIST.forEach((item) => {
