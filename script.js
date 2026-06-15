19eab9444ae71e541ee1bf011d75ada333961d23
ji19star <24243ai@hvhs.school.nz>
2026-06-16 11:00:12 +1200

    ssss

--- script.js
+++ script.js
@@ -36,12 +36,13 @@ const PRODUCTS = [
         price: 12,
     }
 ];
-
+// the arrary which contains the items in cart
 const PURCHASE_LIST = [];
 
 
 // init the products html aka like makes em there kinds
 let products = "";
+let productsInCart = "";
 
 /**********
 MAIN CODE
@@ -52,11 +53,15 @@ for (let id in PRODUCTS) {
     products += `
     <div>
         <h3>${PRODUCTS[id].name}</h3>
-        <div>$${PRODUCTS[id].price}</div>
-        <button onclick="addToCart('${id}')">Add to Cart</button>
+        <div>${PRODUCTS[id].price}</div>
+        <button onclick="addToCart('${id}'), console.log(PURCHASE_LIST)">Add to Cart</button>
     </div>
     `
 }
+
+
+
+
 // inject the products html
 PRODUCTS_CONTAINER.innerHTML = products;
 
@@ -67,36 +72,62 @@ PRODUCTS_CONTAINER.innerHTML = products;
 /**********
 FUNCTIONS
 ***********/
-
+// .. you removed the backtix cuz u forogot what it does rmebebr  that
 // global function - no const or let
 // puts the items into the array and the calculates the prices
 addToCart = function (id) {
     PURCHASE_LIST.push(PRODUCTS[id]);
+    //PURCHASE_LIST.push(PRODUCTS[id]);           //calculates price n puts shows it
     document.getElementById("cart").innerHTML = CALC_TOTAL(PURCHASE_LIST)
+    document.getElementById("itemList").innerHTML = PURCHASE_LIST;
+    console.log(PURCHASE_LIST)
+
 }
 
-//anon , prints the stuffs onto the pages
+
+//anon , prints the stuffs onto theSS pages
+
+//what was this again??
 const NAME_PROD = () => {
     let total = 0;
     PURCHASE_LIST.forEach((item) => {
         total += parseFloat(item.name);
-    }); ``
+    });
     return total
 }
 
-//anon function   calculates the prices of the items
+const NAME_PRINT = () => {
+
+    PURCHASE_LIST.forEach((item) => {
+        print(item.name)
+         
+    });
+
+}
+
+
+// amount of thing total in cart
+const QUANT = () => {
+    let total = 0;
+    PURCHASE_LIST.forEach((item) => {
+        total += !parseFloat(item.name);
+    });
+    return total
+}
+
+
+//anon function   calculates the prices of the itemsSSS
 const CALC_TOTAL = () => {
     let total = 0;
     PURCHASE_LIST.forEach((item) => {
         total += parseFloat(item.price);
-    }); ``
+    });
     return total
 }
 
 
 
-
- //go to top of the pagw
+//go to top of the pagw
 const TOP_FUNCTION = () => {
     console.log("clicked");
     document.body.scrollTop = 0;
